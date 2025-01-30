5cf94b2a8af260871b10125a08850c80300a993f
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-01-29 19:13:02 -0800

    Slappable objects now damage the player

--- slappables/slappable_obj.gd
+++ slappables/slappable_obj.gd
@@ -4,6 +4,14 @@ class_name SlappableObj extends RigidBody3D
 
 func _ready() -> void:
 	slappable.init($MeshInstance3D);
+	
+	self.contact_monitor = true;
+	self.max_contacts_reported = 1;
+	self.body_entered.connect(body_hit);
+	
+func body_hit(body : PhysicsBody3D):
+	if body is Player:
+		body.damage(linear_velocity.length());
 
 func slap(pos, intensity, flash):
 	slappable.pre_slap(flash);
@@ -11,4 +19,3 @@ func slap(pos, intensity, flash):
 	var to = global_position - pos;
 	to.y = 0;
 	self.apply_impulse(to.normalized() * intensity, pos - global_position);
-	
