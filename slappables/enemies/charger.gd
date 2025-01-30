bdb6b4a7075917303ac6650bbfb1fec58ff2fab8
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-01-29 18:03:13 -0800

    Updating charger behavior

--- slappables/enemies/charger.gd
+++ slappables/enemies/charger.gd
@@ -2,13 +2,21 @@ class_name Charger extends Enemy
 
 @onready var shapecast : ShapeCast3D = $ShapeCast3D;
 
+@onready var timer : Timer = $StandTimer;
+
 enum States {
 	IDLE,
 	SEARCHING,
+	STAND_AND_PLAY_ANIM,
 	ATTACKING,
-	COOLDOWN,
+	MOVE_BACK,
 	SLAPPED
 };
+
+func _ready() -> void:
+	super();
+	
+	timer.timeout.connect(stand_done);
 
 var active_state : States = States.IDLE;
 
@@ -27,7 +35,10 @@ func eval_behavior(delta: float):
 				
 		States.SEARCHING:
 			if nav_agent.is_target_reached():
-				active_state = States.ATTACKING;
+				active_state = States.STAND_AND_PLAY_ANIM;
+				next_state = States.ATTACKING;
+				
+				timer.start(0.5);
 				anim_tree.set("parameters/conditions/run", false);
 				anim_tree.set("parameters/conditions/shoot", true);
 				
@@ -51,8 +62,12 @@ func eval_behavior(delta: float):
 			var colliding = shapecast.is_colliding();
 				
 			if charge_travelled > 10 or colliding:
-				active_state = States.COOLDOWN;
+				active_state = States.STAND_AND_PLAY_ANIM;
+				next_state = States.MOVE_BACK;
+				timer.start(1);
+				
 				anim_tree.set("parameters/conditions/shoot", false);
+				anim_tree.set("parameters/conditions/idle", true);
 				velocity = Vector3.ZERO;
 				
 				if colliding:
@@ -62,5 +77,23 @@ func eval_behavior(delta: float):
 							c.damage(20);
 						elif c is Enemy or c is SlappableObj:
 							c.slap(global_position, (charge_travelled + 1) * 1000);
+		States.MOVE_BACK:
+			if player.global_position.distance_to(global_position) > 10:
+				active_state = States.SEARCHING;
+				anim_tree.set("parameters/conditions/run", true);
+			
+			var dir = (global_position - player.global_position).normalized();
+			dir.y = 0;
+			
+			velocity = dir * 4;
+			
+			rotation.y = atan2(dir.x, dir.z);
+		States.STAND_AND_PLAY_ANIM:
+			return
 		States.SLAPPED:
 			return;
+
+var next_state : States = States.IDLE;
+
+func stand_done():
+	active_state = next_state;
