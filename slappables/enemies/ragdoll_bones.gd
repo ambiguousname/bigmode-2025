c400b3bdc3c681ee85b88042aa8fb2d4542a93f0
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-01-31 12:22:56 -0800

    Make slapping bones less intense, fix bug with bone slapping direction

--- slappables/enemies/ragdoll_bones.gd
+++ slappables/enemies/ragdoll_bones.gd
@@ -4,15 +4,17 @@ class_name RagdollBones extends PhysicalBoneSimulator3D
 
 var slappable : Slappable;
 
+const INTENSITY_MUL : float = 0.5;
+
 func ragdoll(slappable : Slappable, pos : Vector3, intensity : float):
 	self.slappable = slappable;
 	
 	active = true;
 	physical_bones_start_simulation();
 	
-	var to = global_position - pos;
+	var to = bone_push.global_position - pos;
 	to.y = 0;
-	bone_push.apply_impulse(to.normalized() * intensity, pos - global_position);
+	bone_push.apply_impulse(to.normalized() * intensity * INTENSITY_MUL, pos - bone_push.global_position);
 	
 func slap(pos : Vector3, intensity : float, flash: bool):
 	if !active:
@@ -20,7 +22,7 @@ func slap(pos : Vector3, intensity : float, flash: bool):
 	
 	slappable.pre_slap(flash);
 	
-	var to = global_position - pos;
+	var to = bone_push.global_position - pos;
 	to.y = 0;
 	
-	bone_push.apply_impulse(to.normalized() * intensity, pos - global_position);
+	bone_push.apply_impulse(to.normalized() * intensity * INTENSITY_MUL, pos - bone_push.global_position);
