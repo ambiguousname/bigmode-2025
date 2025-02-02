1633fc9e9f977ecf00af39cb9b7e6d2710c8a02d
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-02-02 11:09:17 -0800

    Free enemies after next two triggers

--- spawn_trigger.gd
+++ spawn_trigger.gd
@@ -1,12 +1,18 @@
-extends Area3D
+class_name SpawnTrigger extends Area3D
 
 @export var to_spawn : Array[Enemy];
 
 func _ready() -> void:
 	body_entered.connect(_body_entered);
-	
+
 func _body_entered(b : PhysicsBody3D):
 	if b is Player:
 		for e in to_spawn:
 			e.spawn();
-		self.queue_free();
+		CheckpointManager.add_to_free(self);
+		body_entered.disconnect(_body_entered);
+
+func free_stuff():
+	for e in to_spawn:
+		e.queue_free();
+	self.queue_free();
--- tower/checkpoint_manager.gd
+++ tower/checkpoint_manager.gd
@@ -7,3 +7,11 @@ func set_pos(p: Vector3):
 
 func get_respawn_point() -> Vector3:
 	return checkpoint_pos;
+
+var trigger_free_stack : Array[SpawnTrigger];
+
+func add_to_free(trigger : SpawnTrigger):
+	trigger_free_stack.push_front(trigger);
+	if trigger_free_stack.size() > 2:
+		var s : SpawnTrigger = trigger_free_stack.pop_back();
+		s.free_stuff();
