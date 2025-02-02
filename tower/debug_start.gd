5a8fbc3547b97438a2e7777fddeb2ff3d435d4d3
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-02-02 15:18:55 -0800

    Debug hides start

--- tower/debug_start.gd
+++ tower/debug_start.gd
@@ -4,6 +4,8 @@ extends Node3D
 
 func _ready() -> void:
 	if OS.has_feature("editor") and debug:
+		get_tree().current_scene.get_node("CanvasLayer").timer.timeout.emit();
+		
 		get_tree().current_scene.get_node("Player").global_position = global_position;
 		
 		get_tree().current_scene.get_node("Player").global_rotation = global_rotation;
